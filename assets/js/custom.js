78fba9fa122ebf2bcf2e2cd8ef78c60d38afba4e
coderpbt <yeasin800398@gmail.com>
2023-01-25 02:19:50 +0600

    update both tt

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,60 +1,52 @@
-$(document).ready(function(){
+$(document).ready(function () {
+	// slideToggle for search bar js
+	$(".is-mobile-search img").click(function () {
+			$(".mobile-search-box").slideToggle();
+			return false;
+	});
 
-$('.is-mobile-search img').click(function(){
-	$('.mobile-search-box').slideToggle();
-	return false;
-});	
+	// mobile menu js
+	var menuTrigger = $(".js-menuToggle");
+	var topNav = $(".js-topPushNav");
+	var openLevel = $(".js-openLevel");
+	var closeLevel = $(".js-closeLevel");
+	var closeLevelTop = $(".js-closeLevelTop");
+	var navLevel = $(".js-pushNavLevel");
 
-	var menuTrigger = $('.js-menuToggle');
-  var topNav = $('.js-topPushNav');
-  var openLevel = $('.js-openLevel');
-  var closeLevel = $('.js-closeLevel');
-  var closeLevelTop = $('.js-closeLevelTop');
-  var navLevel = $('.js-pushNavLevel');
+	function openPushNav() {
+			topNav.addClass("isOpen");
+			$("body").addClass("pushNavIsOpen");
+	}
 
-function openPushNav() {
-  topNav.addClass('isOpen');
-  $('body').addClass('pushNavIsOpen');
-}
+	function closePushNav() {
+			topNav.removeClass("isOpen");
+			openLevel.siblings().removeClass("isOpen");
+			$("body").removeClass("pushNavIsOpen");
+	}
 
-function closePushNav() {
-  topNav.removeClass('isOpen');
-  openLevel.siblings().removeClass('isOpen');
-  $('body').removeClass('pushNavIsOpen');
-}
+	menuTrigger.on("click touchstart", function (e) {
+			e.preventDefault();
+			if (topNav.hasClass("isOpen")) {
+					closePushNav();
+			} else {
+					openPushNav();
+			}
+	});
 
-menuTrigger.on('click touchstart', function(e) {
-  e.preventDefault();
-  if (topNav.hasClass('isOpen')) {
-    closePushNav();
-  } else {
-    openPushNav();
-  }
+	openLevel.on("click touchstart", function () {
+			$(this).next(navLevel).addClass("isOpen");
+	});
+
+	closeLevel.on("click touchstart", function () {
+			$(this).closest(navLevel).removeClass("isOpen");
+	});
+
+	closeLevelTop.on("click touchstart", function () {
+			closePushNav();
+	});
+
+	$(".screen").click(function () {
+			closePushNav();
+	});
+	
 });
-
-openLevel.on('click touchstart', function(){
-  $(this).next(navLevel).addClass('isOpen');
-});
-
-closeLevel.on('click touchstart', function(){
-  $(this).closest(navLevel).removeClass('isOpen');
-});
-
-closeLevelTop.on('click touchstart', function(){
-  closePushNav();
-});
-
-$('.screen').click(function() {
-    closePushNav();
-});
-
-
-});
-
-
-
-		
-
-
-
-
